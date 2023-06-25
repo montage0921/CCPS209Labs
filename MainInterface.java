b2623ce3bf14fab9866f8eb7a86c9d483a5bded4
montage0921 <garyshi0921@gmail.com>
2023-06-25 16:35:24 -0400

    Remove MainInrerface.java class

--- MainInterface.java
+++
@@ -1,8 +0,0 @@
-public class MainInterface {
-    public static void main(String[] args){
-        int[] coefficients={2,3,4};
-        Polynomial testPolynomial=new Polynomial(coefficients);
-        System.out.println("hello");
-
-    }
-}
