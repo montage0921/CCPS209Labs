de645fc5f2624009ea9321792cac91d8d8deeed5
montage0921 <garyshi0921@gmail.com>
2023-06-27 07:50:40 -0400

    Finish Time Problem

---
+++ TimeProblems.java
@@ -0,0 +1,54 @@
+import java.time.*;
+import java.time.temporal.Temporal;
+
+public class TimeProblems {
+    public static int countFridayThirteens(LocalDate start, LocalDate end){
+        int count=0;
+
+
+        while(!start.isAfter(end)){
+            if(start.getDayOfWeek()==DayOfWeek.FRIDAY&&start.getDayOfMonth()==13){
+                count++;
+            }
+
+            start=start.plusDays(1);
+
+        }
+
+
+
+        return count;
+
+    }
+
+    public static String dayAfterSeconds(
+            LocalDateTime timeHere, long seconds
+    ){
+
+        LocalDateTime timeAfter= timeHere.plusSeconds(seconds);
+
+
+
+        return timeAfter.getDayOfWeek().toString();
+
+    }
+
+    public static int whatHourIsItThere(
+            LocalDateTime timeHere, String here, String there
+    ){
+        ZoneId zoneHere = ZoneId.of(here);
+        ZoneId zoneThere = ZoneId.of(there);
+        ZonedDateTime timeHereInHereZone = timeHere.atZone(zoneHere);
+        ZonedDateTime timeHereInThereZone = timeHereInHereZone.withZoneSameInstant(zoneThere);
+        return timeHereInThereZone.getHour();
+
+    }
+
+    public static void main(String[] args){
+       LocalDateTime timeHere=LocalDateTime.now();
+       whatHourIsItThere(timeHere,"America/Toronto","Pacific/Marquesas");
+
+
+    }
+
+}
