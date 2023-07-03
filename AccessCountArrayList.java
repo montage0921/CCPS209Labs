3b4265824fd59460f26f37a147ae279a7eb1c1b6
montage0921 <garyshi0921@gmail.com>
2023-07-03 13:01:26 -0400

    Finish No.7 AccessCountArrayList

---
+++ AccessCountArrayList.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+
+public class AccessCountArrayList<E> extends ArrayList<E> {
+    int count;
+
+    @Override
+    public E get(int index){
+        this.count++;
+        return super.get(index);
+    }
+
+    @Override
+    public E set(int index, E element){
+        this.count++;
+        return super.set(index,element);
+
+    }
+    public int getAccessCount(){
+        return this.count;
+    }
+
+    public void resetCount(){
+        this.count=0;
+    }
+
+
+
+}
